82d69eb186da29d1024c0cf95b0b7ba2eda43f95
Corentin Poupry <corentin.poupry@outlook.com>
2018-05-25 17:40:39 +0200

    Add TS test

---
+++ test/example.ts
@@ -0,0 +1,11 @@
+import hyperid from '..'
+
+const urlSafeInstance = hyperid({ fixedLength: false, urlSafe: true })
+const fixedLengthInstance = hyperid(true)
+
+const urlSafeId = urlSafeInstance()
+const fixedLengthId = fixedLengthInstance()
+
+// decode
+console.log(hyperid.decode(urlSafeId))
+console.log(fixedLengthInstance.decode(fixedLengthId))
