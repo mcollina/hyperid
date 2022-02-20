8121fed63417dbf48d48c70ab7bfd93a849e2172
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2022-02-20 08:20:11 +0200

    adding startFrom option to example

--- test/example.ts
+++ test/example.ts
@@ -2,10 +2,13 @@ import * as hyperid from '..'
 
 const urlSafeInstance = hyperid({ fixedLength: false, urlSafe: true })
 const fixedLengthInstance = hyperid(true)
+const startFromInstance = hyperid({ startFrom: 9 })
 
 const urlSafeId = urlSafeInstance()
 const fixedLengthId = fixedLengthInstance()
+const startFromId = startFromInstance()
 
 // decode
 console.log(hyperid.decode(urlSafeId))
 console.log(fixedLengthInstance.decode(fixedLengthId))
+console.log(startFromInstance.decode(startFromId))
