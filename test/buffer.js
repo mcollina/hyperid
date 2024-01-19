60204ad0749ed97cba3ae335e5900f922770ff0b
Matteo Collina <hello@matteocollina.com>
2024-01-19 16:20:35 +0100

    cleanup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/buffer.js
+++ test/buffer.js
@@ -3,13 +3,13 @@ const test = require('tape')
 const proxyquire = require('proxyquire')
 
 test('require buffer', function (t) {
-  t.plan(2);
+  t.plan(2)
 
   const instance = hyperid()
   const id = instance()
   t.ok(id)
 
-  proxyquire('../hyperid', { 'buffer': { Buffer: null } });
+  proxyquire('../hyperid', { buffer: { Buffer: null } })
   const instance2 = hyperid()
   const id2 = instance2()
   t.ok(id2)
